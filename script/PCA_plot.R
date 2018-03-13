b793c5acab3c5b5199c20d3ed661f5d48a50f77f
klwang <dzq123456789@163.com>
2018-03-13 21:54:24 +0800

    change pca code to pca function

--- script/PCA_plot.R
+++ script/PCA_plot.R
@@ -4,18 +4,24 @@
 ##Environment: R3.3
 
 #step-by-step pca plot
-pcdata <- princomp(data,cor=T)
-pdf("pca1.pdf")
-plot(pcdata$loadings,pch=18)
-text(pcdata$loadings,substring(row.names(pcdata$loadings),8),pos=4,cex=.7)
-# text(pcdata$loadings,sub("SAMPLE_"," ",rownames(pcdata$loadings)),pos=4,cex=.7)
-dev.off()
+normalPCA <- function(data, main="PCA plot"){
+	pcdata <- princomp(data,cor=T)
+	# pdf("pca1.pdf")
+	plot(pcdata$loadings,pch=18,main=main)
+	text(pcdata$loadings,substring(row.names(pcdata$loadings),8),pos=4,cex=.7)
+	# text(pcdata$loadings,sub("SAMPLE_"," ",rownames(pcdata$loadings)),pos=4,cex=.7)
+	# dev.off()
+}
+
 
 #one step pca auto-plot
-datat <- t(data) 
-alldata <- data.frame(datat,trait)
-library(ggfortify)
-pcaplot <- autoplot(prcomp(alldata[,c(1:nrow(data))]),data=alldata,colour="individual.trait",label=TRUE)
-pdf("pca2.pdf")
-plot(pcaplot)
-dev.off()
+onestepPCA <- function(data, trait, colour, label=FALSE){
+	datat <- t(data) 
+	alldata <- data.frame(datat,trait)
+	# suppressMessages(library(ggfortify))
+	pcaplot <- autoplot(prcomp(alldata[,c(1:nrow(data))]),data=alldata,colour=colour,label=label)
+	# pdf("pca2.pdf")
+	plot(pcaplot)
+	# dev.off()
+}
+#Usage: onestepPCA(data=data,trait=trait_data,colour="Sex",label=TRUE)
