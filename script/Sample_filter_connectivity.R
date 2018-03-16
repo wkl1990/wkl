e7b5ba2fb11ee322d8a945ce8ea63bab538b4409
klwang <dzq123456789@163.com>
2018-03-16 10:50:01 +0800

    bug fix

--- script/Sample_filter_connectivity.R
+++ script/Sample_filter_connectivity.R
@@ -19,7 +19,7 @@ print(paste("There are ",sum(outliers)," outliers samples based on a bicor dista
 # print(table(outliers))
 plot(Z.K, col = as.numeric(datMeta$study), pch=19, main="Outlier detection", ylab="Network connectivity (z score)")
 # plot(Z.K, col = as.numeric(outliers)+1, pch=19, main="Outlier detection", ylab="Network connectivity (z score)")
-plabline(h=-2, lty=2)
+abline(h=-2, lty=2)
 # abline(h=c(2,-2), lty=2)
 legend("bottomleft",legend = levels(datMeta$study), col=1:7,pch=19,cex=.7)
 # legend("bottomleft",legend = c("non-outliers","outliers"), col=1:2,pch=19,cex=.7)
